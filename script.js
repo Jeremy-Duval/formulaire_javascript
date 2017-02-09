9da142c358b609e405faee10b9d4b8dd87b7db65
Chatmalow <maxime.pie@etu.univ-lyon1.fr>
2017-02-09 10:24:04 +0100

    added new features
    we now know when the checkbox is checked or not and react in the different cases

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
 //Wait for window to be loaded before you execute that code 
 window.addEventListener("load", function() {
+
+//Action on button	
 console.log(document.getElementById("target"));
 document.getElementById("target").addEventListener("click", function() {
-	console.log('haha, tickles'); 
-});
+	console.log('haha, hey'); 
+	
 });
 
-		function test() {
-		
-			console.log('test');
+//Action on checkbox 
+console.log(document.getElementById("check")); 
+document.getElementById("check").addEventListener("click", check);	
+	 
+});
+
+
+		function check() {
+				if(document.getElementById("check").checked)
+				{
+					console.log("Vous m'avez cochée");
+				}
+				else 
+				{
+					console.log("Vous m'avez bien décochée comme il faut !"); 
+				}
 		}
 	
