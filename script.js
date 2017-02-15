bd1e64c901dada37f8529f04e25769e26c364020
Duval Jérémy <p1501022@univ-lyon1.fr>
2017-02-15 14:05:30 +0100

    xrdsg

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 
 //Wait for window to be loaded before you execute that code 
 window.addEventListener("load", function() {
-console.log(document.getElementById("target"));
-document.getElementById("target").addEventListener("click", function() {
-	console.log('haha, tickles'); 
+	console.log(document.getElementById("submit"));
+	document.getElementById("age").addEventListener("onclick", function() {
+		console.log('haha, tickles'); 
+	});
 });
-});
-
-		function test() {
-		
-			console.log('test');
-		}
 	
