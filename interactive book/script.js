a1f698bd9c404bcadc4f8d3f8c7bf77ce4362b56
Duval Jérémy <p1501022@univ-lyon1.fr>
2017-03-08 18:01:07 +0100

    a js piste

--- interactive book/script.js
+++ interactive book/script.js
@@ -4,7 +4,25 @@ window.addEventListener("load", function() {
 });
 
 function getPage(){
-	var page = JSON.stringify("json/chapitre1.json");
-	document.getElementById("demo").innerHTML = page.txt;
-	console.log(page.txt);
+	var req = new XMLHttpRequest();
+	var url = "json/chapitre1.json";
+	req.open("GET", url, true);
+	req.onerror = function() {
+	    console.log("Échec de chargement "+url);
+	};
+	
+	req.onload = function() {
+		console.log("ok");
+	    if (req.status === 200) {
+		    var page = JSON.parse(req.responseText);
+			console.log(data.txt);
+
+			document.getElementById("demo").innerHTML = page.txt;
+	      	// do what you have to do with 'data'
+	    } else {
+	      	console.log("Erreur " + req.status);
+	    }
+	};
+
+	
 }
