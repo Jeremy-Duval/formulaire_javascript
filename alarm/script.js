1617495bf0aaaba31739bd2a2b0599db4e39b7b3
Duval Jérémy <p1501022@univ-lyon1.fr>
2017-03-08 14:45:44 +0100

    maj : not complete

--- alarm/script.js
+++ alarm/script.js
@@ -6,16 +6,18 @@ window.addEventListener("load", function() {
 	 
 });
 
+num_id=0;
 
 function createAlarm(){
-
+	num_id++;
+	console.log("i : "+num_id);
 	/*****************************************************************************************/
 
     var checkbox_alarm = document.createElement('input');
 	checkbox_alarm.type = "checkbox";
 	checkbox_alarm.name = "checkAlarm";
 	checkbox_alarm.value = "alarmOk";
-	checkbox_alarm.id = "checkAlarm";
+	checkbox_alarm.id = "checkAlarm"+num_id;
 
 	var number_hours = document.createElement('input');
 	number_hours.type = "number";
@@ -23,7 +25,7 @@ function createAlarm(){
 	number_hours.value = "0";
 	number_hours.min = "0";
 	number_hours.max = "23";
-	number_hours.id = "hoursAlarm";
+	number_hours.id = "hoursAlarm"+num_id;
 
 	var number_minutes = document.createElement('input');
 	number_minutes.type = "number";
@@ -31,19 +33,19 @@ function createAlarm(){
 	number_minutes.value = "0";
 	number_minutes.min = "0";
 	number_minutes.max = "59";
-	number_minutes.id = "minutesAlarm";
+	number_minutes.id = "minutesAlarm"+num_id;
 
 	var text_name = document.createElement('input');
 	text_name.type = "text";
 	text_name.name = "nameAlarm";
 	text_name.value = "WAKE UP... please";
-	text_name.id = "nameAlarm";
+	text_name.id = "nameAlarm"+num_id;
 
 	var button_remove = document.createElement('input');
 	button_remove.type = "button";
 	button_remove.name = "removeAlarm";
 	button_remove.value = "-";
-	button_remove.id = "removeAlarm";
+	button_remove.id = "removeAlarm"+num_id;
 
 	/*****************************************************************************************/
 	//Create array of options to be added
@@ -53,7 +55,7 @@ function createAlarm(){
 	var select_sound = document.createElement('select');
 	select_sound.type = "select";
 	select_sound.name = "soundAlarm";
-	select_sound.id = "soundAlarm";
+	select_sound.id = "soundAlarm"+num_id;
 
 	//Create and append the options
 	for (var i = 0; i < array.length; i++) {
@@ -79,5 +81,17 @@ function createAlarm(){
 	fieldset_alarm.appendChild(button_remove);
 
 	document.form.appendChild(fieldset_alarm);
+
+
+	/******************************************EventListener**************************************/
+	button_remove.addEventListener('click', function(evt){
+
+		var alarmDelete = evt.target.value;
+		try{
+
+		}catch(err){
+			console.log("Error : there is a problem to delete the alarm, but don't worry : we work on !");
+		}
+	});
     
 }
