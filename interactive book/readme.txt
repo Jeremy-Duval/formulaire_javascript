a8c0b6ee8c31d867976352450cb01eba35755f22
Jérémy-Duval <jeremy-duval1@hotmail.fr>
2017-03-15 16:01:10 +0100

    interactiv book end

---
+++ interactive book/readme.txt
@@ -0,0 +1,2 @@
+http://iutdoua-web.univ-lyon1.fr/~p1506348/interactive%20book/
+
--- interactive book/script.js
+++ interactive book/script.js
@@ -1,23 +1,42 @@
+
+var url;
 window.addEventListener("load", function() {
+	url = "json/chapitre1.json";
 	getPage();
 	 
 });
 
+
+window.addEventListener("hashchange", function()
+{
+	url = "json/chapitre"+location.hash.substring(1,location.hash.length)+".json"; 
+	console.log(url);
+	getPage(); 
+	
+	
+}); 
 function getPage(){
+	
+	//AJAX call
 	var req = new XMLHttpRequest();
-	var url = "json/chapitre1.json";
+	
 	req.open("GET", url, true);
 	req.onerror = function() {
 	    console.log("Échec de chargement "+url);
 	};
 	
-	req.onload = function() {
-		console.log("ok");
-	    if (req.status === 200) {
-		    var page = JSON.parse(req.responseText);
-			console.log(data.txt);
+req.onload = function() {
 
+	if (req.status === 200)
+		{
+		    var page = JSON.parse(req.responseText);
+			//get link id
+			console.log(page.links[0].txt);
+			
+			//get link txt 
 			document.getElementById("demo").innerHTML = page.txt;
+			link(page); 
+
 	      	// do what you have to do with 'data'
 	    } else {
 	      	console.log("Erreur " + req.status);
@@ -27,4 +46,39 @@ function getPage(){
 	req.send();
 
 	
+}
+
+var numId = 0; 
+var parentLink = 0;
+
+//gets a parsed objects, and makes a link from it
+function link(page)
+{
+	var i;
+	var listOldLink;
+	numId ++;
+	
+	console.log(page.links.length);
+	
+	i=0;
+	while(i<parentLink){
+		oldLink = document.getElementById(i);
+		oldLink.remove();
+		i++;
+	}
+	
+	var length = page.links.length;
+	parentLink = length;	
+	i = 0;
+	while(i < length){
+		var linky = document.createElement('a');
+		var theLink = page.links[i].link;
+		url = theLink; 
+		linky.href = theLink; 
+		linky.innerHTML = page.links[i].txt; 
+		linky.id = i;
+		document.body.appendChild(linky); 
+		i++;
+	}
+
 }
