057684ad84bf7a6d79d1af39db1c0c8d8a1d8016
Duval Jérémy <p1501022@univ-lyon1.fr>
2017-03-22 15:27:12 +0100

    "A little step for a man, a huge step for the humanity" Maxime

--- podcastReader/script.js
+++ podcastReader/script.js
@@ -1,4 +1,33 @@
 
 window.addEventListener("load", function() {
-
+    getXML();
 });
+
+
+function getXML(){
+    
+    var url = "http://pandamixshow.com/podcast/dubstep/mix/podcast.xml/";
+
+    //AJAX call
+    var req = new XMLHttpRequest();
+    
+    req.open("GET", url, true);
+    req.onerror = function() {
+        console.log("Échec de chargement "+url);
+    };
+    
+    req.onload = function() {
+
+        if (req.status === 200)
+            {
+                console.log("OK ! Good Job !");
+
+                // do what you have to do with 'data'
+            } else {
+                console.log("Erreur " + req.status);
+            }
+        };
+
+        req.send();
+};
+    
