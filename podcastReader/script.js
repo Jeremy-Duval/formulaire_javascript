45a37b163b7c6a60f2de7c91d72e89d640f87c70
Duval Jérémy <p1501022@univ-lyon1.fr>
2017-03-22 15:50:12 +0100

    Parser ? Need to know how to use it...

--- podcastReader/script.js
+++ podcastReader/script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function() {
 
 function getXML(){
     
-    var url = "http://pandamixshow.com/podcast/dubstep/mix/podcast.xml/";
+    var url = "https://crossorigin.me/https://itunes.apple.com/fr/podcast/00-your-name-et-la-garde-royale/id1192681583?i=1000379721885&mt=2";
 
     //AJAX call
     var req = new XMLHttpRequest();
@@ -22,7 +22,12 @@ function getXML(){
             {
                 console.log("OK ! Good Job !");
 
-                // do what you have to do with 'data'
+                parser = new DOMParser();
+                var xml_flux = parser.parseFromString(req.responseText,"text/xml");
+
+                console.log("------------------------");
+                console.log("title      : " + xml_flux);
+
             } else {
                 console.log("Erreur " + req.status);
             }
